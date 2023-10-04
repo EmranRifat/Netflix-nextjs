2e4cf6aaecb86b128e56ccd9cb32d43d69b9a85b
EmranRifat <you@example.comimran15-1919@diu.edu.bd>
2023-10-04 18:01:40 +0600

    data error

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,8 +1,24 @@
 import Link from 'next/link';
 import React from 'react';
 
-const movie = () => {
+const movie = async () => {
+
+ const url = process.env.RAPID_KEY;
+  
+
+ const options = {
+   method: 'GET',
+   headers: {
+     'X-RapidAPI-Key': '689fb8073emshe831d33ef1be31bp1938f8jsn5655cba19256',
+     'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+   }
+ };
+ const res=await fetch(url,options);
+ const data=await res.json();
+ console.log(data);
+   
     return (
+        
         <div>
             <h1>Movies all...</h1>
             <Link href="movie/100" >
